214b0f9d0715ab82c18a7ca0a89651ed88b90633
adikresna122 <adikresna122@gmail.com>
2025-05-20 17:59:59 +0700

    Ulang Migrasi, Ubah Permissions dan Testing OTP

--- backend/sparklore/sparklore/settings.py
+++ backend/sparklore/sparklore/settings.py
@@ -66,6 +66,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny', 
+    ),
 }
 
 AUTH_USER_MODEL = "authentification.CustomUser"
