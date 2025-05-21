39e01b9f0d3202460ab583a070f9ec49ee2818a1
adikresna122 <adikresna122@gmail.com>
2025-05-21 17:43:43 +0700

    Validate Models and Serializers

--- backend/sparklore/api/urls.py
+++ backend/sparklore/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CharmViewSet, OrderViewSet, ProductViewSet, CartViewSet, ReviewViewSet, NewsletterSubscriberViewSet
+from .views import CharmViewSet, OrderViewSet, ProductViewSet, CartViewSet, ReviewViewSet, NewsletterSubscriberViewSet, checkout
 
 router = DefaultRouter()
 router.register(r'charms', CharmViewSet, basename='charm')
@@ -12,4 +12,6 @@ router.register(r'cart', CartViewSet, basename='cart')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('checkout/', checkout, name='checkout'),
+
 ]
